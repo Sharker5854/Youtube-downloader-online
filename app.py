03e970ed9cb4c025fa91ec84c9ed511682077775
Vladimir <vladimir@Best-Komp.localdomain>
2022-08-07 16:05:09 +0300

    Finished a front-end side on index-page AND implemented a mechanism of searching video and it's metadata by ajax.

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, url_for, jsonify
+from pytube import YouTube
+from pytube.exceptions import RegexMatchError, VideoUnavailable
+
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", get_video_metadata_url=url_for("get_video_metadata"))
+
+@app.route("/validate-link/", methods=["POST"])
+def get_video_metadata():
+    try:
+        pytube_obj = YouTube(url=request.json["current_url"])
+    except RegexMatchError:
+        return jsonify({"fail_message" : "Ссылка на YouTube-видео указана неверно!"})
+    else:
+        try:
+            return jsonify({
+                "video_title" : pytube_obj.title,
+                "thumbnail_url" : pytube_obj.thumbnail_url
+            })
+        except VideoUnavailable: # user can pass url for another site, so pytube won't raise RegexMatchError or smth else, it just won't be able to get video's metadata
+            return jsonify({"fail_message" : "Ссылка на YouTube-видео указана неверно!"})
 
 @app.route("/install/", methods=["POST"])
 def install_video():
--- static/script.js
+++ static/script.js
@@ -1,7 +1,60 @@
 window.onload = function() {
-    let link_input = document.querySelector("#link-input")
 
-    link_input.addEventListener("input", function(){
-        // AJAX-запрос - поиск видео по текущей введенной ссылке
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    let url_input = document.querySelector("#url-input")
+    let found_video_block = document.querySelector(".found-video-data")
+    let found_video_title = document.querySelector("#video-title")
+    let found_video_thumbnail = document.querySelector("#video-thumbnail")
+
+    if ("current-form-value" in localStorage) {
+        url_input.value = JSON.parse(localStorage.getItem("current-form-value"))
+    }
+
+    url_input.addEventListener("input", function(){
+        if (url_input.value != "") {
+            localStorage.setItem("current-form-value", JSON.stringify(url_input.value))
+            $.ajax({
+                url: get_video_metadata_url,
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: JSON.stringify({
+                    "current_url" : url_input.value
+                }),
+                success: function(response) {
+                    found_video_title.style.color = "#FFFFFF"
+                    if ("fail_message" in response) {
+                        found_video_block.style.display = "none"
+                        sleep(50).then(() => {
+                            found_video_title.innerHTML = response["fail_message"]
+                            found_video_thumbnail.removeAttribute("src")
+                            found_video_block.style.display = "block"
+                        });
+                    }
+                    else {
+                        found_video_block.style.display = "none"
+                        sleep(30).then(() => {
+                            found_video_title.innerHTML = response["video_title"]
+                            found_video_thumbnail.src = response["thumbnail_url"]
+                            found_video_block.style.display = "block"
+                        });  
+                    }
+                },
+                error: function(error) {
+                    found_video_title.style.color = "#FF0000"
+                    found_video_title.innerHTML = "Ошибка. Что-то пошло не так :("
+                    found_video_thumbnail.removeAttribute("src")
+                }
+            });
+        }
+        else {
+            found_video_title.innerHTML = ""
+        }
     });
+
+
 };
