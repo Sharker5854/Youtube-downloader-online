6e6a73dccfe8cd88f65b72221a980e7eeae17ad0
Vladimir <vladimir@Best-Komp.localdomain>
2022-08-09 21:02:27 +0300

    Implemented downloading video-files by ajax AND the frontend has been improved.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tempfile
 import io
+from colorama import Back
 from flask import Flask
 from flask import request, render_template, url_for, jsonify, send_file
 from pytube import YouTube
@@ -12,7 +13,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html", get_video_metadata_url=url_for("get_video_metadata"))
+    return render_template(
+        "index.html", 
+        get_video_metadata_url=url_for("get_video_metadata"),
+        install_video_url=url_for("install_video")
+    )
 
 
 @app.route("/validate-link/", methods=["POST"])
@@ -25,7 +30,8 @@ def get_video_metadata():
         try:
             return jsonify({
                 "video_title" : pytube_obj.title,
-                "thumbnail_url" : pytube_obj.thumbnail_url
+                "transliterated_title" : iuliia.translate(pytube_obj.title, iuliia.TELEGRAM),
+                "thumbnail_url" : pytube_obj.thumbnail_url,
             })
         except VideoUnavailable: # user can pass url for another site, so pytube won't raise RegexMatchError or smth else, it just won't be able to get video's metadata
             return jsonify({"fail_message" : "Ссылка на YouTube-видео указана неверно!"})
@@ -33,20 +39,22 @@ def get_video_metadata():
 
 @app.route("/install/", methods=["POST"])
 def install_video():
-    # yt = YouTube(url=request.json["video_url"])
-    yt = YouTube(url="https://www.youtube.com/watch?v=EGtxv5mWCi0")
-    stream_obj = yt.streams.get_by_itag(22) # 720p / 30fps by default
+    try:
+        yt = YouTube(url=request.json["video_url"])
+        stream_obj = yt.streams.get_by_itag(22) # 720p / 30fps by default
 
-    with tempfile.NamedTemporaryFile(mode="w+b", prefix="yt_", suffix="_video", dir="video_tempfiles/") as temp:
-        video_binary_buffer = io.BytesIO()
-        stream_obj.stream_to_buffer(video_binary_buffer)
-        video_binary_buffer.seek(0)
-        temp.write(video_binary_buffer.getvalue()) # method .getvalue() returns 'bytes-like' object instead of '_ioBytes' obj
-        temp.seek(0)
+        with tempfile.NamedTemporaryFile(mode="w+b", prefix="yt_", suffix="_video", dir="video_tempfiles/") as temp:
+            video_binary_buffer = io.BytesIO()
+            stream_obj.stream_to_buffer(video_binary_buffer)
+            video_binary_buffer.seek(0)
+            temp.write(video_binary_buffer.getvalue()) # method .getvalue() returns 'bytes-like' object instead of '_ioBytes' obj
+            temp.seek(0)
 
-        return send_file(
-            temp.name, as_attachment=True, download_name=f"{iuliia.translate(yt.title, iuliia.TELEGRAM)}_YouTubeInstaller.mp4"
-        )
+            return send_file(
+                temp.name, as_attachment=True
+            )
+    except Exception as exc:
+        print(Back.RED + f"Server Error: {exc}")
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -1,21 +1,22 @@
 window.onload = function() {
 
+    const url_input = document.querySelector("#url-input")
+    const submit_input = document.querySelector("#submit-input")
+    const found_video_block = document.querySelector(".found-video-data")
+    const found_video_title = document.querySelector("#video-title")
+    const found_video_thumbnail = document.querySelector("#video-thumbnail")
+    let last_found_video_title = NaN
+
 
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
-    }
+    };
 
-    let url_input = document.querySelector("#url-input")
-    let submit_input = document.querySelector("#submit-input")
-    let found_video_block = document.querySelector(".found-video-data")
-    let found_video_title = document.querySelector("#video-title")
-    let found_video_thumbnail = document.querySelector("#video-thumbnail")
+    // function show_install_process_spinner() {
+        
+    // };
 
-    if ("current-form-value" in localStorage) {
-        url_input.value = JSON.parse(localStorage.getItem("current-form-value"))
-    }
-
-    url_input.addEventListener("input", function(){
+    function get_video_metadata_by_ajax() {
         if (url_input.value != "") {
             localStorage.setItem("current-form-value", JSON.stringify(url_input.value))
             $.ajax({
@@ -43,15 +44,20 @@ window.onload = function() {
                             found_video_title.innerHTML = response["video_title"]
                             found_video_thumbnail.src = response["thumbnail_url"]
                             found_video_block.style.display = "block"
+                            last_found_video_title = response["transliterated_title"]
                         });
                         submit_input.removeAttribute("disabled")
                     }
                 },
                 error: function(error) {
-                    found_video_title.style.color = "#FF0000"
-                    found_video_title.innerHTML = "Ошибка. Что-то пошло не так :("
+                    found_video_block.style.display = "none"
                     found_video_thumbnail.removeAttribute("src")
                     submit_input.disabled = true
+                    sleep(30).then(() => {
+                        found_video_title.style.color = "#FF0000"
+                        found_video_title.innerHTML = "Ошибка. Что-то пошло не так :("
+                        found_video_block.style.display = "block"
+                    });
                 }
             });
         }
@@ -61,7 +67,50 @@ window.onload = function() {
             submit_input.disabled = true
             localStorage.setItem("current-form-value", JSON.stringify(""))
         }
-    });
+    };
 
+    function install_video_by_ajax() {
+        console.log("Кручу спиннер!")
+        $.ajax({
+            url: install_video_url,
+            type: "POST",
+            async: true,
+            contentType: "application/json; charset=utf-8",
+            dataType: "binary",
+            xhrFields: {
+                "responseType" : "blob"
+            },
+            data: JSON.stringify({
+                "video_url" : url_input.value
+            }),
+            success: function(blob_obj, status, xhr) {
+                var link = document.createElement("a")
+                link.href = window.URL.createObjectURL(blob_obj)
+                link.download = last_found_video_title + "_YouTubeInstaller.mp4"
+                link.click()
+            },
+            error: function(error) {
+                found_video_block.style.display = "none"
+                found_video_thumbnail.removeAttribute("src")
+                sleep(30).then(() => {
+                    found_video_title.style.color = "#FF0000"
+                    found_video_title.innerHTML = "На сервере произошла ошибка. Пожалуйста попробуйте позже."
+                    found_video_block.style.display = "block"
+                });
+            }
+        });
+    };
+
+
+    if ("current-form-value" in localStorage) {
+        url_input.value = JSON.parse(localStorage.getItem("current-form-value"))
+    }
+    if (url_input.value != "") {
+        get_video_metadata_by_ajax();
+    }
+
+
+    url_input.addEventListener("input", get_video_metadata_by_ajax);
+    $("#submit-input").on("click", install_video_by_ajax);
 
 };
